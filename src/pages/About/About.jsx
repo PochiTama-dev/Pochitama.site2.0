579a3748ece3e9266bbde68ef8a491cd18f71583
“Pablo <braguepablo@gmail.com>
2023-09-15 17:29:58 -0300

    progreso en seccion colaboradores

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,10 +4,25 @@ import "../../Themes/Base/_variables.css"
 
 const About = () => {
   return (
-    <section className='aboutSection'>
-      <div className='aboutLine1'></div>
-      <h1 className='aboutTitle'>Quiénes Somos</h1>
-      <div className='aboutLine2'></div>
+    <section>
+      <section className='aboutSection'>
+        <div className='aboutLine1'></div>
+        <h1 className='aboutTitle'>Quiénes Somos</h1>
+        <div className='aboutLine2'></div>
+      </section>
+      <section className='equipoPochitama'>
+        <div className='colaboradores'>
+          <div className='colaborador'>
+            <div className='encabezado'>
+              <img src="https://avatars.githubusercontent.com/u/111319458?v=4" alt="" />
+            </div>
+            <div className='info'>
+              <h4>GONZALA ENRICA</h4>
+              <h5>Fullstack Developer</h5>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   )
 }
