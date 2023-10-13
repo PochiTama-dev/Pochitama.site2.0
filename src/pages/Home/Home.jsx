2a97de40759d7b16a32e5665bbcd03416a0cd59d
Brague Pablo <braguepablo@gmail.com>
2023-10-13 10:50:01 -0300

    Creacion de section 2 con texto y estilado de descripcion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,12 @@ const Home = () => {
         <h1 className='beneTitle'>¿POR QUÉ ELEGIRNOS?</h1>
         <div className='beneLine2'></div>
       </section>
+      <section className='beneSection2'>
+        <div className='beneDescription'>
+          <h3 className='beneDescriptionText'>Descripción de la consultoría destacando los beneficios</h3>
+          <h3 className='beneDescriptionText2'>y la propuesta de valor única de la consultoría en el campo de desarrollo de aplicaciones</h3>
+        </div>
+      </section>
     </section>
   )
 }
