46f391bfc688b0b36620ea5b1a72ae66be14cb4b
Brague Pablo <braguepablo@gmail.com>
2023-10-13 10:08:40 -0300

    Creacion de Titulo y Aplicacion de Background

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import "./home.css"
+import "../../Themes/Base/_variables.css";
 
 const Home = () => {
   return (
-    <div>  Home!</div>
+    <section className='beneFont'>
+      <section className='aboutSection'>
+        <div className='aboutLine1'></div>
+        <h1 className='aboutTitle'>¿POR QUÉ ELEGIRNOS?</h1>
+        <div className='aboutLine2'></div>
+      </section>
+    </section>
   )
 }
 
