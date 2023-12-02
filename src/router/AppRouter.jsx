b374b87aee083211d383eca0b433a5bb6e82e136
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2023-12-02 02:23:09 -0300

    (Update) change to vite

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import AboutUs from '../pages/About/About';
-import Services from '../pages/Services/Services';
-import Portfolio from '../pages/Portfolio/Portfolio';
-import Contact from '../pages/Contact/Contact';
-import Header from '../components/header/Header';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import AboutUs from "../pages/About/About";
+import Services from "../pages/Services/Services";
+import Portfolio from "../pages/Portfolio/Portfolio";
+import Contact from "../pages/Contact/Contact";
+import Header from "../components/header/Header";
 import Footer from "../components/footer/footer";
 
 const AppRouter = () => (
   <Router>
-    <Header/>
+    <Header />
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/sobre-nosotros" element={<AboutUs/>} />
-      <Route path="/servicios" element={<Services/>} />
-      <Route path="/portfolio" element={<Portfolio/>} />
-      <Route path="/contacto" element={<Contact/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre-nosotros" element={<AboutUs />} />
+      <Route path="/servicios" element={<Services />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/contacto" element={<Contact />} />
     </Routes>
-    <Footer/>
+    <Footer />
   </Router>
 );
 
