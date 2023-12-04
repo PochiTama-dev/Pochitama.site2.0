93343efdaed456acd9eccdaf702f54b64220fc6c
matidiaz032 <maty_032@hotmail.es>
2023-12-04 19:34:20 -0300

    colores del menu mobile

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import "./contact.css";
+import "../../Themes/Base/_variables.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contactFont">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <h1 className="contactTitle">¡Vamos a charlar sobre tu proyecto!</h1>
+          </div>
+          <div className="col-8">
+            <h2 className="contactSubtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, aut rem. Eveniet laborum quae inventore.</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <span>Estas buscando:</span>
+          </div>
+          <div className="col-sm-10">
+            
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
