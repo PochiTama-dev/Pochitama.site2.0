69ad9d64bf19ef829e310def07bd2917215072da
matidiaz032 <maty_032@hotmail.es>
2023-12-06 23:34:30 -0300

    Vista Contacto

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,28 +1,60 @@
+import { Button, Col, Container, Row, Form } from "react-bootstrap";
+import { RiMailSendLine } from "react-icons/ri";
 import "./contact.css";
 import "../../Themes/Base/_variables.css";
 
 const Contact = () => {
   return (
-    <section className="contactFont">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <h1 className="contactTitle">¡Vamos a charlar sobre tu proyecto!</h1>
-          </div>
-          <div className="col-8">
-            <h2 className="contactSubtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, aut rem. Eveniet laborum quae inventore.</h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12">
-            <span>Estas buscando:</span>
-          </div>
-          <div className="col-sm-10">
-            
-          </div>
-        </div>
-      </div>
-    </section>
+    <Container fluid className="bg-primary p-5">
+      <Row className="ps-md-4 ps-md-4 ps-lg-5 ms-lg-5" >
+        <Col xs={12}>
+          <h2 className="text-bold text-white text-uppercase">¡Vamos a charlar sobre tu proyecto!</h2>
+        </Col>
+        <Col xs={10} md={9} lg={5}>
+          <h3 className="contactSubtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, aut rem. Eveniet laborum quae inventore.</h3>
+        </Col>
+      </Row>
+      <Row className="ps-md-4 ps-md-4 ps-lg-5 ms-lg-5" >
+        <Col xs={12} className="text-white mb-3">
+          <span>Estas buscando:</span>
+        </Col>
+        <Col xs={12} md={8} lg={5} className="mb-4">
+        <Button variant="tertiary" className="text-white px-4 me-1 my-1" size="lg">SEO</Button>{' '}
+        <Button variant="tertiary" className="text-white px-4 mx-1 my-1" size="lg">Web design & dev</Button>{' '}
+        <Button variant="tertiary" className="text-white px-4 mx-1 my-1" size="lg">App  design & dev</Button>{' '}
+        <Button variant="tertiary" className="text-white px-4 me-1 my-1" size="lg">Branding</Button>{' '}
+        <Button variant="tertiary" className="text-white px-4 mx-1 my-1" size="lg">Design Context</Button>{' '}
+        </Col>
+      </Row>
+
+      <Row className="ps-md-4 ps-md-4 ps-lg-5 ms-lg-5" >
+        <Col xs={12} md={10} lg={4}>
+          <Form >
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label className="text-white">¿Cual es tu nombre?</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="text-white">Tu E-mail</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicProject">
+              <Form.Label className="text-white">Contanos sobre tu proyecto</Form.Label>
+              <Form.Control type="textarea" />
+            </Form.Group>
+            <div className="text-end mt-4">
+              <Button variant="tertiary" className="text-white text-uppercase" type="submit">
+                <RiMailSendLine className="me-2 mb-1" size={25} />
+                Enviar formulario
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
