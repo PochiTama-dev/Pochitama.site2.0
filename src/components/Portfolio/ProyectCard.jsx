8360895298df34dea67de76265a530bed609b2db
Agustin Kassargian <akassargian@brown.gob.ar>
2023-12-07 15:57:55 -0300

    PT-06 // MODAL

---
+++ src/components/Portfolio/ProyectCard.jsx
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+import ModalPortfolio from './ModalPortfolio';
+
+function ProyectCard({proyectName, position, link}) {
+    const [show, setShow]  = useState(false);
+    const handleShowModal = () =>{
+        setShow(!show)
+    }
+    const handleCLickCard = () =>{
+        if(link) window.open(link, '_blank');
+        else{
+            handleShowModal()
+        }
+    }
+
+    return (
+        <div className={`porfolio-proyect-box ${position}`} onClick={handleCLickCard}>{/*FONDO CON IMAGEN*/}
+            <div className='portfolio-pildora'>{/*PILDORA PROYECTO*/}
+                <label className='portfolio-label'>{proyectName}</label>
+            </div>
+            {show === true && <ModalPortfolio show={show}/>}
+    </div>
+    );
+}
+
+export default ProyectCard;
