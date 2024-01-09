feb3b7af4bfab5193f94c04d41a56d3ebd26226f
Agustin Kassargian <akassargian@brown.gob.ar>
2024-01-09 14:55:17 -0300

    Mod carousel

--- src/components/quienes/AboutUsSlider.jsx
+++ src/components/quienes/AboutUsSlider.jsx
@@ -3,28 +3,42 @@ import { Carousel } from 'react-bootstrap';
 
 const AboutUsSlider = ({ equipo }) => {
   const [index, setIndex] = useState(0);
+  const [sheet, setSheet] = useState([equipo[0],equipo[1],equipo[2]])
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 3 < equipo.length ? prevIndex + 3 : 0));
-    }, 3000); // Ajusta el valor según la velocidad de cambio que desees
-    console.log(equipo)
-    return () => clearInterval(intervalId);
-  }, [equipo]);
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setIndex((prevIndex) => (prevIndex + 1 < Math.ceil(equipo.length / 3) ? prevIndex + 1 : 0));
+  //   }, 3000);
+
+  //   return () => clearInterval(intervalId);
+  // }, [equipo]);
+
+  useEffect(()=>{
+    setSheet([equipo[index+3], equipo[index+4], equipo[index+5]])
+  },[index])
+
+  useEffect(()=>{
+    console.log(sheet[0])
+  },[sheet])
 
   return (
     <Carousel activeIndex={index} onSelect={() => {}}>
-       {equipo && equipo.map((miembro, i) => (
-        i >= index && i < index + 3 && (
-          <Carousel.Item key={i}>
-            <img className="d-block w-100" src={miembro.imagen} alt={miembro.nombre} />
-            {/* <Carousel.Caption>
-              <h3>{miembro.nombre}</h3>
-              <p>{miembro.area}</p>
-            </Carousel.Caption> */}
-          </Carousel.Item>
-        )
-      ))}
+      {equipo &&
+        sheet.map((item, i) => {
+          const groupIndex = Math.floor(i / 3);
+          return groupIndex === index && (
+            <Carousel.Item key={i}>
+              <img src={item[0]?.imagen} alt={item[0]?.nombre} />
+              <img src={item[1]?.imagen} alt={item[1]?.nombre} />
+              <img src={item[2]?.imagen} alt={item[2]?.nombre} />
+              {/* Puedes incluir el siguiente código para mostrar el nombre y área */}
+              {/* <Carousel.Caption>
+                <h3>{miembro.nombre}</h3>
+                <p>{miembro.area}</p>
+              </Carousel.Caption> */}
+            </Carousel.Item>
+          );
+        })}
     </Carousel>
   );
 };
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import "./about.css";
 import "../../Themes/Base/_variables.css";
 import colaboradores from "../../colaboradores.json";
 import Cta from "../../components/cta/Cta";
-import AboutUsSlider
- from "../../components/quienes/AboutUsSlider.JSX";
+import AboutUsSlider from "../../components/quienes/AboutUsSlider.jsx";
 const About = () => {
   const titleCta = "¿Queres que tu proyecto se expanda?";
   const subtitleCta =
     "¡Es posible! \n Nuestros expertos en E-commerce te pueden ayudar.";
+
+    const [equipo, setEquipo] = useState()
+    useEffect(()=>{
+      setEquipo(colaboradores.equipo)
+    },[])
   return (
     <div>
       <Cta title={titleCta} subtitle={subtitleCta} />
@@ -20,9 +24,12 @@ const About = () => {
         </section>
         <section className="equipoPochitama">
           {colaboradores.equipo && (
-             <AboutUsSlider
-            equipo = {colaboradores.equipo}
-            />
+            <div style={{maxHeight:"40vh", border:'2px solid red'}}>
+              <AboutUsSlider
+             equipo = {equipo}
+             setEquipo = {setEquipo}
+             />
+            </div>
           )}
          
           {/* <div className="colaboradores">
