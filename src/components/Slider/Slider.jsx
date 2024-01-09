d3e91cd5aed54838bb00cff2aaef969cbe04f90a
NachoMaraude <maraudenacho@gmail.com>
2024-01-09 00:34:03 -0300

    PT-13 // Slider mobile

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,6 +6,7 @@ import "./slider.css";
 import line from "../../assets/images/Line.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import logo from "../../assets/images/LogoPochi.png";
 
 function CustomSlider({ title = "¿Qué dicen nuestros Clientes?" }) {
   const sliderRef = useRef(null);
@@ -22,26 +23,38 @@ function CustomSlider({ title = "¿Qué dicen nuestros Clientes?" }) {
     }
   };
 
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="slider">
       <div className="slider-title-box">
-        <img src={line} alt="line" />
+        <img src={line} alt="line" className="line-left" />
         <h1 className="slider-title">{title}</h1>
-        <img src={line} alt="line" />
+        <img src={line} alt="line" className="line-right" />
       </div>
 
-      <Slider
-        ref={sliderRef}
-        className="carousel"
-        infinite={true}
-        speed={500}
-        slidesToShow={3}
-        slidesToScroll={1}
-      >
+      <Slider ref={sliderRef} className="carousel" {...settings}>
         {loremIpsum.map((e, i) => (
-          <div key={i} className="card-container">
-            <div className="item" onClick={goToNextSlide}>
-              <p className="item-text">{e.name}</p>
+          <div key={i} className="slider-card-container">
+            <div className="slider-logo-container">
+              <img src={logo} alt="logo" className="slider-logo" />
+            </div>
+            <div className="slider-item" onClick={goToNextSlide}>
+              <p className="slider-item-text">{e.name}</p>
             </div>
           </div>
         ))}
