9b58f5c4ab70b1b9cfa8800c162ec18989430608
AgustinKassargian <agustinkassargian@gmail.com>
2024-01-16 20:39:17 -0300

    PT-15 / CSS modal responsive

--- src/components/quienes/AboutUsSlider.jsx
+++ src/components/quienes/AboutUsSlider.jsx
@@ -1,45 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel, Container, Row, Col, Image } from 'react-bootstrap';
+import '../quienes/aboutusslider.css'
 
-const AboutUsSlider = ({ equipo }) => {
+const AboutUsSlider = ({ equipo, screenWidth }) => {
+  const [index, setIndex] = useState(0);
+
   if (!equipo || equipo.length === 0) {
     return <p>No hay información de equipo disponible.</p>;
   }
 
   const itemsPerSlide = 3;
+  const totalSlides = Math.ceil(equipo.length / itemsPerSlide);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  const handleNext = () => {
+    // Si estamos en la última hoja, reiniciar el carousel
+    if (index === totalSlides - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
   const carouselItems = [];
 
-  for (let index = 0; index < Math.ceil(equipo.length / itemsPerSlide); index++) {
-    const startIndex = index * itemsPerSlide;
-    const endIndex = startIndex + itemsPerSlide;
-
-    const currentSlice = equipo.slice(startIndex, endIndex);
-    const missingItems = itemsPerSlide - currentSlice.length;
-
-    // Agregar elementos ficticios si faltan
-    for (let i = 0; i < missingItems; i++) {
-      currentSlice.push({ nombre: 'Proximamente', imagen: 'https://avatars.githubusercontent.com/u/111319458?v=4' });
+  if(screenWidth > 768){
+    for (let i = 0; i < totalSlides; i++) {
+      const startIndex = i * itemsPerSlide;
+      const endIndex = startIndex + itemsPerSlide;
+      const currentSlice = equipo.slice(startIndex, endIndex);
+      const missingItems = itemsPerSlide - currentSlice.length;
+      for (let j = 0; j < missingItems; j++) {
+        currentSlice.push({ nombre: 'Proximamente', imagen: 'https://avatars.githubusercontent.com/u/111319458?v=4' });
+      }
+      carouselItems.push(
+        <Carousel.Item key={i}>
+          <Row>
+            {currentSlice.map((miembro, idx) => (
+              <Col key={idx} md={4}  className="d-flex justify-content-center align-items-center">
+                <div className='slider_box'>
+                  <div className='avatar_box'>
+                    <Image className='about_avatar' src={miembro.imagen} alt={miembro.nombre} roundedCircle fluid />
+                  </div>
+                  <div className="info">
+                    <text>{miembro.nombre}</text>
+                    <h5>{miembro.area}</h5>
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Carousel.Item>
+      );
     }
-
-    carouselItems.push(
-      <Carousel.Item key={index}>
-        <Row>
-          {currentSlice.map((miembro, idx) => (
-            <Col key={idx} md={4}>
-              <Image src={miembro.imagen} alt={miembro.nombre} roundedCircle fluid />
-              <p>{miembro.nombre}</p>
-            </Col>
-          ))}
-        </Row>
-      </Carousel.Item>
-    );
   }
-
+  else{
+    equipo.forEach((miembro, idx) => {
+      carouselItems.push(
+        <Carousel.Item key={"C"+idx}>
+          <Row>
+              <Col key={idx} md={4} className="d-flex justify-content-center align-items-center">
+                <div className='slider_box'>
+                  <div className='avatar_box'>
+                    <Image className='about_avatar' src={miembro.imagen} alt={miembro.nombre} roundedCircle fluid />
+                  </div>
+                  <div className="info">
+                    <text>{miembro.nombre}</text>
+                    <h5>{miembro.area}</h5>
+                  </div>
+                </div>
+              </Col>
+          </Row>
+        </Carousel.Item>
+      );
+    });
+  }
   return (
     <Container>
       <Row>
         <Col>
-          <Carousel indicators={false} interval={null} wrap={false}>
+          <Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
+            indicators={false}
+            interval={null}
+            wrap={false}
+          >
             {carouselItems}
           </Carousel>
         </Col>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,47 +5,72 @@ import colaboradores from "../../colaboradores.json";
 import Cta from "../../components/cta/Cta";
 import AboutUsSlider from "../../components/quienes/AboutUsSlider"
 const About = () => {
+  
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const updateScreenWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateScreenWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateScreenWidth);
+    };
+  }, []);
+
   const titleCta = "¿Queres que tu proyecto se expanda?";
   const subtitleCta =
     "¡Es posible! \n Nuestros expertos en E-commerce te pueden ayudar.";
 
-    const [equipo, setEquipo] = useState(colaboradores.equipo)
-    useEffect(()=>{
-      setEquipo(colaboradores.equipo)
-    },[])
   return (
     <div>
       <Cta title={titleCta} subtitle={subtitleCta} />
+      { screenWidth > 1024 &&
+          <section className="why_section">
+            <section className="d-flex justify-content-center align-items-center w-100">
+              <div className="aboutLine1"style={{backgroundColor:'white'}}/>
+              <h1 className="">¿POR QUÉ ELEGIRNOS?</h1>
+              <div className="aboutLine2"/>
+            </section>
+            <section>
+              <text>
+              Descripción de la cons ultoría destacando los beneficios y la propuesta de valor única de la consultoría en el campo de desarrollo de aplicaciones
+              </text>
+            </section>
+          </section>
+      }
       <section>
         <section className="aboutSection">
-          <div className="aboutLine1"/>
-          <h1 className="aboutTitle">QUIÉNES SOMOS</h1>
-          <div className="aboutLine2"/>
+          { screenWidth > 1024 && <div className="aboutLine1"/>}
+          <h1 className="aboutTitle">¿QUIÉNES SOMOS?</h1>
+          {screenWidth > 1024 && <div className="aboutLine2"/>}
         </section>
-        <section className="equipoPochitama">
-          {colaboradores.equipo && (
-            <div style={{maxHeight:"40vh", border:'2px solid red'}}>
-              <AboutUsSlider
-             equipo = {equipo}
-             setEquipo = {setEquipo}
-             />
-            </div>
-          )}
-         
-          {/* <div className="colaboradores">
-            {colaboradores.equipo.map((colaborador, index) => (
-              <div key={index} className="colaborador">
-                <div className="encabezado">
-                  <img src={colaborador.imagen} alt={colaborador.nombre} />
-                </div>
-                <div className="info">
-                  <h4>{colaborador.nombre}</h4>
-                  <h5>{colaborador.area}</h5>
-                </div>
-              </div>
-            ))}
-          </div> */}
-        </section>
+        { screenWidth >= 1024 ? 
+                  <section className="equipoPochitama">
+                  <div className="colaboradores">
+                    {colaboradores.equipo.map((colaborador, index) => (
+                      <div key={index} className="colaborador">
+                        <div className="box">
+                          <div className="encabezado">
+                            <img src={colaborador.imagen} alt={colaborador.nombre} />
+                          </div> 
+                          <div className="info">
+                            <h4>{colaborador.nombre}</h4>
+                            <h5>{colaborador.area}</h5>
+                        </div>
+                      </div>
+                        </div>
+                    ))}
+                  </div>
+                </section>
+        :
+          <AboutUsSlider
+              equipo={colaboradores.equipo}
+              screenWidth={screenWidth}
+          />
+        }
+
       </section>
     </div>
   );
