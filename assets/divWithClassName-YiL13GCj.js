cb96a68b4a7a8bf47528bc0a3dbe3dd920e08c35
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-02-05 18:43:47 -0300

    Updates

---
+++ assets/divWithClassName-YiL13GCj.js
@@ -0,0 +1,1 @@
+import{r as e,j as m}from"./index-iGG6lv7c.js";import{d as o}from"./Button-k_6sgzIr.js";const c=a=>e.forwardRef((s,r)=>m.jsx("div",{...s,ref:r,className:o(s.className,a)}));export{c as d};
