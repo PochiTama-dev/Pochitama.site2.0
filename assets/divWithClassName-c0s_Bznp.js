f264f1069aebfd4b08fcb1e2415c2f8d8f1eac9e
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-02-05 18:22:56 -0300

    Updates

---
+++ assets/divWithClassName-c0s_Bznp.js
@@ -0,0 +1,1 @@
+import{r as e,j as m}from"./index-xs2K1FtB.js";import{d as o}from"./Button-rhWHzPjy.js";const c=a=>e.forwardRef((s,r)=>m.jsx("div",{...s,ref:r,className:o(s.className,a)}));export{c as d};
