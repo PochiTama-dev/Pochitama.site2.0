401e34cd8ef9091d6bb53e6e5beaca717187090b
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-02-19 11:05:34 -0300

    Updates

---
+++ assets/divWithClassName-VsIVAgPK.js
@@ -0,0 +1,1 @@
+import{r as e,j as m}from"./index-rcWzsKhM.js";import{d as o}from"./Button-g1j8bFON.js";const c=a=>e.forwardRef((s,r)=>m.jsx("div",{...s,ref:r,className:o(s.className,a)}));export{c as d};
