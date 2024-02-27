4373cdb910a0adc2340569b7fb3be64c139f6f2f
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-02-27 13:24:30 -0300

    Updates

---
+++ assets/divWithClassName-9vKwKM_-.js
@@ -0,0 +1,1 @@
+import{r as e,j as m}from"./index-vfStjfDb.js";import{d as o}from"./Button-h7SKzuDi.js";const c=a=>e.forwardRef((s,r)=>m.jsx("div",{...s,ref:r,className:o(s.className,a)}));export{c as d};
