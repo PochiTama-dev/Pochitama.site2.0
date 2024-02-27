6034180595433e76dffe36dcc19569ae9970b72a
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-02-27 12:12:02 -0300

    imagenes clientes

--- src/components/Slider/variables.js
+++ src/components/Slider/variables.js
@@ -2,27 +2,43 @@ export const titleClients = "¿Que dicen nuestros Clientes?";
 
 export const loremIpsum = [
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "Contrate el servicio de desarrolo web con Pochitama por recomendacion, y ha sido un servicio maravilloso. el tiempo de respuesta, el entendimiento de lo que buscaba, todo es excelente.",
+    img: "https://ilum-mexico.com/wp-content/uploads/2023/07/cropped-Logo-Ilum-2-120x131.jpg",
+    fir: "Ilum Mexico"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "Impecable la atencion y servicio que me han brindado, acompañamiento personalizado, empatia y entendimiento de mis requerimientos y posibilidades. trato humano por sobre todas las cosas.",
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_m8YotlO1x6BI3cRX9vIJ-FOHVE94yZwZMdC3X962o=s176-c-k-c0x00ffffff-no-rj",
+    fir: "Nutriciom Espiritual"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "¡Gracias por ayudarnos con nuestra web!. Fueron muy profesionales y atentos ante todo.",
+    img: "https://dcdn.mitiendanube.com/stores/003/220/925/themes/common/logo-1217000551-1694281722-bf8763a8f14e909c415b6e02601eb0791694281722.ico?0",
+    fir: "Mahuer"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "Increíblemente satisfecho con LeeloAca.com. Su servicio al cliente es de primera, siempre dispuestos a ayudar y aclarar dudas.",
+    img: "https://leeloaca.com/wp-content/uploads/2023/03/WhatsApp-Image-2023-03-30-at-08.48.32.jpeg",
+    fir: "LeeloAca"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "Lavariso transformo la forma en que gestionamos nuestro negocio. Su software CRM/ERP se adapta perfectamente a nuestras necesidades. El soporte técnico es genial",
+    img: "https://instagram.feze12-1.fna.fbcdn.net/v/t51.2885-19/280930348_970996156898849_7470730095545767848_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.feze12-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=I7-H5HxSahsAX_djEH2&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfCr0Me86PL7b_-PsEhODeEVd7Ss4iYODlP3iTRiJIGtOA&oe=65E2A3E7&_nc_sid=8b3546",
+    fir: "Grupo Service"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "La rapidez y eficiencia de nuestra pagina nos ha salvado en más de una ocasión. muy recomendable el servicio de Pochitama.dev ",
+    img: "../../assets/images/Clientes/Abar.png",
+    fir: "Cerrajeria Abar"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "Ribrox es una herramienta clave en la gestión de nuestras redes. Su software es robusto y fácil de manejar, mejorando nuestra productividad.",
+    img: "../../assets/images/Clientes/incuba4d.jpg",
+    fir: "Incuba4D"
   },
   {
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut etmassa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapienfringilla, mattis ligula consectetur, ultrices mauris.",
+    name: "La asesoría en desarrollo web para Kindom.com.ar fue sublime. Captaron perfectamente nuestra esencia, creando un sitio tanto bello como funcional. gracias.",
+    img: "https://kindom.com.ar/wp-content/uploads/2023/11/logo-kindom.jpg",
+    fir: "Kindom Idiomas"
   },
 ];
