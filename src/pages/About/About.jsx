5ccb8d3ea9f09e683505c77a2fe60c046d852b6f
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-04-12 16:13:44 -0300

    fotos about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import "./about.css";
 import "../../Themes/Base/_variables.css";
 import colaboradores from "../../colaboradores.json";
